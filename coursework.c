8a042b5e48fe52710a6356211b01586f34ce00c7
Lisa Ho <lisahoyenxin@gmail.com>
2022-12-20 22:41:56 +0800

    Updated comparision function for MLFQ

--- coursework.c
+++ coursework.c
@@ -1,6 +1,5 @@
 /*
 G52OSC Coursework
-
 Group 3:
 Chai Qin Hui 20307184
 Kelly Tan Kai Ling 20310184
@@ -21,6 +20,8 @@ float total_tat;
 float avg_turn_FCFS;
 float avg_wait_RR;
 float avg_turn_RR;
+float avg_wait_MLFQ;
+float avg_turn_MLFQ;
 
 // functions for input validation
 int checkInput();
@@ -37,7 +38,7 @@ float avgTurnTime(int processes[], int n, int bt[], int wt[], int tat[]);
 void avgtime_RR(int process[], int n, int burst[], int timequantum);
 void algorithm_RR(int process[], int n, int burst[], int wait[], int turnaround[], int timequantum);
 
-// functions for FCFS
+// functions for MLFQ
 void printMLFQ(int processes[], int n, int bt[], int timequantum);
 
 // function for comparison
@@ -138,7 +139,7 @@ void printFCFS(int processes[], int n, int bt[])
     total_wt = total_wt + wt[i];
     printf("%i\t\t%i\t\t\t%i\t\t\t%i\n", (i + 1), bt[i], tat[i], wt[i]);
   }
-
+  // calculate and print average waiting time and average turnaround time
   float t = (float)total_tat / n;
   float w = (float)total_wt / n;
   avg_turn_FCFS = t;
@@ -230,60 +231,78 @@ void avgtime_RR(int process[], int n, int burst[], int timequantum)
 
 void printMLFQ(int processes[], int n, int bt[], int timequantum)
 {
-    int wait[n], turn[n], turnaround[n], current_burst[n];
-    // flag to check if any process goes to queue 2
-    int flag = 0;
-    // initialize current time (arrival time) to 0
-    int current_time = 0;
-    // copy the initial burst time for every process
+  int wait[n], turn[n], turnaround[n], current_burst[n];
+  // flag to check if any process goes to queue 2
+  int flag = 0;
+  // initialize current time (arrival time) to 0
+  int current_time = 0;
+  // copy the initial burst time for every process
+  for (int i = 0; i < n; i++)
+  {
+    current_burst[i] = bt[i];
+  }
+
+  int total_wait_RR = 0;
+  int total_turn_RR = 0;
+  int total_wait_FCFS = 0;
+  int total_turn_FCFS = 0;
+
+  printf("First Queue: Round Robin (RR)\n");
+  printf("Process\t\tBurst Time\t\tTurnaround Time\t\tWaiting Time\n");
+  // for every process (starting from process 1)
+  for (int i = 0; i < n; i++)
+  {
+    // if still have remainder burst time (process not completed)
+    if (bt[i] > timequantum)
+    {
+      current_time = current_time + timequantum;
+      turnaround[i] = current_time;
+      current_burst[i] -= timequantum;
+      flag = 1;
+    }
+    // if current proccess' burst time < time quantum
+    else
+    {
+      current_time = current_time + bt[i];
+      turnaround[i] = current_time;
+      wait[i] = turnaround[i] - current_burst[i];
+      current_burst[i] = 0;
+      total_wait_RR = total_wait_RR + wait[i];
+      total_turn_RR = total_turn_RR + turnaround[i];
+      printf("%i\t\t%i\t\t\t%i\t\t\t%i\n", i + 1, bt[i], turnaround[i], wait[i]);
+    }
+  }
+  printf("\nSecond Queue: First Come First Served (FCFS)\n");
+  printf("Process\t\tBurst Time\t\tTurnaround Time\t\tWaiting Time\n");
+  if (flag == 1)
+  {
     for (int i = 0; i < n; i++)
     {
-        current_burst[i] = bt[i];
+      if (current_burst[i] != 0)
+      {
+        wait[i] = current_time - turnaround[i];
+        current_time = current_time + current_burst[i];
+        turnaround[i] = current_time;
+        current_burst[i] = 0;
+        total_wait_FCFS = total_wait_FCFS + wait[i];
+        total_turn_FCFS = total_turn_FCFS + turnaround[i];
+        printf("%i\t\t%i\t\t\t%i\t\t\t%i\n", i + 1, bt[i], turnaround[i], wait[i]);
+      }
     }
+  }
+  else
+  {
+    printf("No process in queue 2\n");
+  }
 
-    printf("First Queue: Round Robin (RR)\n");
-    printf("Process\t\tBurst Time\t\tTurnaround Time\t\tWaiting Time\n");
-    // for every process (starting from process 1)
-    for (int i = 0; i < n; i++)
-    {
-      // if still have remainder burst time (process not completed)
-      if (bt[i] > timequantum)
-        {
-          current_time = current_time + timequantum;
-          turnaround[i] = current_time;
-          current_burst[i] -= timequantum;
-          flag = 1;
-        }
-        // if current proccess' burst time < time quantum
-        else
-        {
-          current_time = current_time + bt[i];
-          turnaround[i] = current_time;
-          wait[i] = turnaround[i] - current_burst[i];
-          current_burst[i] = 0;
-          printf("%i\t\t%i\t\t\t%i\t\t\t%i\n", i + 1, bt[i], turnaround[i], wait[i]);
-        }
-    }
-    printf("\nSecond Queue: First Come First Served (FCFS)\n");
-    printf("Process\t\tBurst Time\t\tTurnaround Time\t\tWaiting Time\n");
-    if(flag==1)
-    {
-        for (int i = 0; i < n; i++)
-        {
-            if (current_burst[i] != 0){
-                wait[i] = current_time - turnaround[i];
-                current_time = current_time + current_burst[i];
-                turnaround[i] = current_time;
-                current_burst[i] = 0;
-                printf("%i\t\t%i\t\t\t%i\t\t\t%i\n", i + 1, bt[i], turnaround[i], wait[i]);
-            }
-        }
-
-    } else {
-        printf("No process in queue 2\n");
-    }
-    printf("------------------------------------------------------------------------------------\n");
-
+  // calculate and print average waiting time and average turnaround time
+  float avg_wait = (float)(total_wait_RR + total_wait_FCFS) / n;
+  float avg_turn = (float)(total_turn_RR + total_turn_FCFS) / n;
+  avg_wait_MLFQ = avg_wait;
+  avg_turn_MLFQ = avg_turn;
+  printf("\nAverage turnaround time: %.2f", avg_turn);
+  printf("\nAverage waiting time: %.2f\n", avg_wait);
+  printf("------------------------------------------------------------------------------------\n");
 }
 
 // function used for comparison to find the best one according to the chosen comparison algorithm
@@ -299,82 +318,82 @@ void comparison()
   char best[50];
   float FCFS = avg_wait_FCFS + avg_turn_FCFS;
   float RR = avg_wait_RR + avg_turn_RR;
+  float MLFQ = avg_wait_MLFQ + avg_turn_MLFQ;
   float min;
-  int selection;
 
-  printf("Please choose the algorithm that you want to choose the best algorithm that suit your process:\n1.Average Waiting Time + Average Turn Around Time\n2.Best Average Waiting Time\n3.Best Average Turn Around Time\n");
-  printf("What is your choice: ");
-  scanf("%i", &selection);
-
-  switch (selection)
+  // Find the Best Average Waiting Time + Average Turn Around Time
+  if ((FCFS < RR) && (FCFS < MLFQ))
   {
-    case 1:
-    {
-      if (FCFS < RR)
-      {
-        min = FCFS;
-        strcpy(best, "First Come First Serve");
-        printf("\n\nThe algorithm that have the shortest total average waiting time and turnaround time is %s with time %.2f microseconds.\n\n", best, min);
-      }
-      else if (RR < FCFS)
-      {
-        min = RR;
-        strcpy(best, "Round Robin Without Overhead");
-        printf("\n\nThe algorithm that have the shortest total average waiting time and turnaround time is %s with time %.2f microseconds.\n\n", best, min);
-      }
-      else
-      {
-        min = FCFS;
-        printf("\n\nBoth algorithms have the same total average waiting time and turnaround time, which is %.2f microseconds.\n\n", min);
-      }
-      break;
-    }
-    case 2:
-    {
-      if (avg_wait_FCFS < avg_wait_RR)
-      {
-        min_wait = avg_wait_FCFS;
-        strcpy(best_wait, "First Come First Serve");
-        printf("\n\nThe algorithm that have the shortest average waiting time is %s with time %.2f microseconds.\n\n", best_wait, min_wait);
-      }
-      else if (avg_wait_RR < avg_wait_FCFS)
-      {
-        min_wait = avg_wait_RR;
-        strcpy(best_wait, "Round Robin");
-        printf("\n\nThe algorithm that have the shortest average waiting time is %s with time %.2f microseconds.\n\n", best_wait, min_wait);
-      }
-      else
-      {
-        min_wait = avg_wait_FCFS;
-        printf("\n\nBoth algorithms have the same average waiting time, which is %.2f microseconds.\n\n", min_wait);
-      }
-      break;
-    }
-    case 3:
-    {
-      if (avg_turn_FCFS < avg_turn_RR)
-      {
-        min_turn = avg_turn_FCFS;
-        strcpy(best_turn, "First Come First Serve");
-        printf("\n\nThe algorithm that have the shortest average turnaround time is %s with time %.2f microseconds.\n\n", best_turn, min_turn);
-      }
-      else if (avg_turn_RR < avg_turn_FCFS)
-      {
-        min_turn = avg_turn_RR;
-        strcpy(best_turn, "Round Robin Without Overhead");
-        printf("\n\nThe algorithm that have the shortest average turnaround time is %s with time %.2f microseconds.\n\n", best_turn, min_turn);
-      }
-      else
-      {
-        min_turn = avg_turn_FCFS;
-        printf("\n\nBoth algorithms have the same average waiting time, which is %.2f microseconds.\n\n", min_turn);
-      }
-      break;
-    }
-    default:
-    {
-      printf("Invalid");
-    }
+    min = FCFS;
+    strcpy(best, "First Come First Serve");
+    printf("\nThe algorithm that have the shortest total average waiting time and turnaround time is %s with time %.2f microseconds.\n\n", best, min);
+  }
+  else if ((RR < FCFS) && (RR < MLFQ))
+  {
+    min = RR;
+    strcpy(best, "Round Robin");
+    printf("\nThe algorithm that have the shortest total average waiting time and turnaround time is %s with time %.2f microseconds.\n\n", best, min);
+  }
+  else if ((MLFQ < FCFS) && (MLFQ < RR))
+  {
+    min = MLFQ;
+    strcpy(best, "Multilevel Feedback Queue");
+    printf("\nThe algorithm that have the shortest total average waiting time and turnaround time is %s with time %.2f microseconds.\n\n", best, min);
+  }
+  else
+  {
+    min = FCFS;
+    printf("\nAll algorithms have the same total average waiting time and turnaround time, which is %.2f microseconds.\n\n", min);
+  }
+
+  // Find the Best Average Waiting Time
+  if ((avg_wait_FCFS < avg_wait_RR) && (avg_wait_FCFS < avg_wait_MLFQ))
+  {
+    min_wait = avg_wait_FCFS;
+    strcpy(best_wait, "First Come First Serve");
+    printf("\n\nThe algorithm that have the shortest average waiting time is %s with time %.2f microseconds.\n\n", best_wait, min_wait);
+  }
+  else if ((avg_wait_RR < avg_wait_FCFS) && (avg_wait_RR < avg_wait_MLFQ))
+  {
+    min_wait = avg_wait_RR;
+    strcpy(best_wait, "Round Robin");
+    printf("\nThe algorithm that have the shortest average waiting time is %s with time %.2f microseconds.\n\n", best_wait, min_wait);
+  }
+  else if ((avg_wait_MLFQ < avg_wait_FCFS) && (avg_wait_MLFQ < avg_wait_RR))
+  {
+    min_wait = avg_wait_MLFQ;
+    strcpy(best_wait, "Multilevel Feedback Queue");
+    printf("\nThe algorithm that have the shortest average waiting time is %s with time %.2f microseconds.\n\n", best_wait, min_wait);
+  }
+  else
+  {
+    min_wait = avg_wait_FCFS;
+    printf("\nAll algorithms have the same average waiting time, which is %.2f microseconds.\n\n", min_wait);
+  }
+
+  // Find the Best Average Turn Around Time
+  if ((avg_turn_FCFS < avg_turn_RR) && (avg_turn_FCFS < avg_turn_MLFQ))
+  {
+    min_turn = avg_turn_FCFS;
+    strcpy(best_turn, "First Come First Serve");
+    printf("\nThe algorithm that have the shortest average turnaround time is %s with time %.2f microseconds.\n\n", best_turn, min_turn);
+  }
+  else if ((avg_turn_RR < avg_turn_FCFS) && (avg_turn_RR < avg_turn_MLFQ))
+  {
+    min_turn = avg_turn_RR;
+    strcpy(best_turn, "Round Robin");
+    printf("\nThe algorithm that have the shortest average turnaround time is %s with time %.2f microseconds.\n\n", best_turn, min_turn);
+  }
+  else if ((avg_turn_MLFQ < avg_turn_FCFS) && (avg_turn_MLFQ < avg_turn_RR))
+  {
+    min_turn = avg_turn_MLFQ;
+    strcpy(best_turn, "Multi-Level Feedback Queue");
+    printf("\nThe algorithm that have the shortest average turnaround time is %s with time %.2f microseconds.\n\n", best_turn, min_turn);
+  }
+  else
+  {
+    min_turn = avg_turn_FCFS;
+    printf("\nAll algorithms have the same average waiting time, which is %.2f microseconds.\n\n", min_turn);
   }
 }
 
